ab20fbd80ccfc134dc392bf4423480d5a97d8bba
Crowdbotics <team@crowdbotics.com>
2024-01-29 18:23:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-credit-45672",
+  url: "https://aged-credit-45672.botics.co",
+  api: "https://aged-credit-45672.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
